1eeeffcae314bec415415704b866e672b08599da
Kent Chadwick <kento.mc@gmail.com>
2021-03-02 22:52:11 +0000

    Add next initialization to pseudocode

--- bin/index.js
+++ bin/index.js
@@ -3,14 +3,15 @@
 console.log( 'Set up yer containers ya dolt!' );
 
 // evolve new
-// evolve give project a name
-// evolve specify desired backend and client repo names?
-// evolve specify desired ports
-// copy files
 // check for/install docker & docker-compose
+// evolve give project a name
+// evolve specify desired backend and client ports
+// copy files
+// create client image
 // spin up containers
 // wp cli install wordpress
 // evolve child enter theme details
 // wp scaffold child-theme
 // wp cli install/activate theme/plugin
+// initialize next
 // launch browser windows

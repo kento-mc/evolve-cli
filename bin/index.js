ca6acb02ef2d00a9483b2bf53e1ed37363710237
Kent Chadwick <kento.mc@gmail.com>
2021-03-02 22:45:19 +0000

    Basic wordpress scaffolding set up. docker-compose.yml file sucessfully launching wordpress container

--- bin/index.js
+++ bin/index.js
@@ -1,3 +1,16 @@
 #!/usr/bin/env node
 
 console.log( 'Set up yer containers ya dolt!' );
+
+// evolve new
+// evolve give project a name
+// evolve specify desired backend and client repo names?
+// evolve specify desired ports
+// copy files
+// check for/install docker & docker-compose
+// spin up containers
+// wp cli install wordpress
+// evolve child enter theme details
+// wp scaffold child-theme
+// wp cli install/activate theme/plugin
+// launch browser windows
